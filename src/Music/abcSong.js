17a456363a6bd7fcd58eab0d8a5f4f0571f1fdb0
yairbuch <121622716+yairbuch@users.noreply.github.com>
2024-03-08 07:03:23 +0200

    /

--- src/Music/abcSong.js
+++ src/Music/abcSong.js
@@ -2,15 +2,15 @@ import { musicButton, boxna, i9 } from "../services/domService.js";
 // import * as Tone from "tone";
 
 //pass in some initial values for the filter and filter envelope
-const Tone = require("tone");
+// const Tone = require("tone");
 
-const synth = new Tone.PolySynth(Tone.Synth).toDestination();
-const now = Tone.now();
+// const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+// const now = Tone.now();
 
-synth.triggerAttack("A4", now + 1);
-synth.triggerAttack("C4", now + 1.5);
-synth.triggerAttack("E4", now + 2);
-synth.triggerRelease(["D4", "F4", "A4", "C4", "E4"], now + 4);
+// synth.triggerAttack("A4", now + 1);
+// synth.triggerAttack("C4", now + 1.5);
+// synth.triggerRelease(["D4", "F4", "A4", "C4", "E4"], now + 4);
+// synth.triggerAttack("E4", now + 2);
 
 export const music = async () => {
   if (boxna.value == "abc") {
@@ -161,7 +161,7 @@ export const music = async () => {
   d4.onclick = () => {
     // const faNote = new Audio("../../sounds/f.mp3");
     // faNote.play();
-    synth.triggerAttack("F4", now + 0.5);
+    // synth.triggerAttack("F4", now + 0.5);
   };
   c3.onclick = () => {
     const miNote = new Audio("../../sounds/e.mp3");
@@ -170,7 +170,7 @@ export const music = async () => {
   b2.onclick = () => {
     // const reNote = new Audio("../../sounds/d.mp3");
     // reNote.play();
-    synth.triggerAttack("D4", now);
+    // synth.triggerAttack("D4", now);
   };
   g7.onclick = () => {
     const siNote = new Audio("../../sounds/b.mp3");
